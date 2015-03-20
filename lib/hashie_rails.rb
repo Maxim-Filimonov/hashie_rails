35d98bb093b5a01aa540c323b4f9a5b4cb5961a6
Maxim-Filimonov <tpaktopsp@gmail.com>
2015-03-21 10:55:39 +1100

    add deprecation warnings

--- lib/hashie_rails.rb
+++ lib/hashie_rails.rb
@@ -1,3 +1,4 @@
 require 'hashie_rails/hashie/mash'
+warn '[DEPRECATION] This gem has been renamed to hashie-forbidden_attributes and will no longer be supported. Please switch to hashie-forbidden_attributes as soon as possible.'
 module HashieRails
 end
